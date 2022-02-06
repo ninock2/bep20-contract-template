19d3e763fa9d7f5e3458ca3f860fd454d64b30d7
ninock2 <cehteljkristl.nino@gmail.com>
2022-02-06 13:00:48 +0100

    Commiting initial tokens

---
+++ SafeMoon-Token-Clone.sol
@@ -0,0 +1,9 @@
+You need contract like SafeMoon with features:
+
+   3% fee auto add to the liquidity pool to locked forever when selling
+   2% fee auto distribute to all holders
+   I created a black hole so token will deflate itself in supply with every transaction
+   50% Supply is burned at start.
+   Auto liquid to Pancake v2.
+   
+   Please contact me on telegram @TokoAgus
---
+++ Token-Baby.sol
@@ -0,0 +1,1 @@
+Create Baby Token BSC – BEP20 Token Generator - https://tokoagus.id/jasa-pembuatan-token/bsc/baby-token/
